778ed28ca947e14ee8e8e3bfe61764c6d6c62458
Tuhin Shuvra <tuhinshuvra18@gmail.com>
2025-10-07 16:17:37 +0600

    added some update

---
+++ functions.js
@@ -0,0 +1,23 @@
+
+
+
+
+// function square(a) {
+//     const result = a * a;
+//     console.log('Result = ', result);
+// }
+
+// square(17);
+
+function square(number) {
+    const result = number * number * number;
+    return result;
+}
+
+const output = square(3);
+// console.log('Result = ', output);
+
+console.log(Math.PI);
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.6));
+console.log(Math.round(4.5));
--- jsPractice.js
+++ jsPractice.js
@@ -13,4 +13,18 @@ let reverse = '';
 for (const letter of speech) {
     reverse = letter + reverse;
 }
-console.log(reverse);
+// console.log(reverse);
+
+const friends = ['Elon', 'Bill', 'Shuvra', 'Dinar', 'Jack'];
+const friends2 = ['Silon', 'Gill', 'Answar'];
+const numbers = [4, 67, 23, 1, 8, 28];
+console.log(numbers.sort());
+
+// console.log(friends[0]);
+// console.log(friends.reverse());
+const result = friends.push(friends2);
+// console.log(result);
+for (const friend of friends) {
+    // console.log(friend);
+    // console.log(friend);
+}
